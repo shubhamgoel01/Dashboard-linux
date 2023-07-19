719f5e65f716c56d826a0404b497e9abaed95baa
Shubham Goel <33158343+shubhamgoel01@users.noreply.github.com>
2023-07-19 18:02:52 +0530

    Update views.py

--- project1/Home/views.py
+++ project1/Home/views.py
@@ -22,14 +22,14 @@ import datetime
 
 
 def index(request):
-    crt_day = " /usr/bin/openssl x509 -enddate -noout -in /opt/Dashboard_All/project1/extra/clients.crt  | awk '{print $2}'"
+    crt_day = " /usr/bin/openssl x509 -enddate -noout -in /opt/Dashboard-linux/project1/extra/clients.crt  | awk '{print $2}'"
     crt_day_result = subprocess.check_output(crt_day, shell=True)
     crt_day_1 = crt_day_result.decode('utf-8').rstrip()
-    crt_year = "/usr/bin/openssl x509 -enddate -noout -in /opt/Dashboard_All/project1/extra/clients.crt  | awk '{print $4}'"  # Replace with your desired command
+    crt_year = "/usr/bin/openssl x509 -enddate -noout -in /opt/Dashboard-linux/project1/extra/clients.crt  | awk '{print $4}'"  # Replace with your desired command
     crt_year_result = subprocess.check_output(crt_year, shell=True)
     crt_year_1 = crt_year_result.decode('utf-8').rstrip()
     month_dict = {'Jan': '1', 'Feb': '2', 'Mar': '3', 'Apr': '4', 'May': '5', 'Jun': '6', 'Jul': '7', 'Aug': '8', 'Sep': '9', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
-    crt_month = "/usr/bin/openssl x509 -enddate -noout -in /opt/Dashboard_All/project1/extra/clients.crt  | awk -F '=' '{print $2}' | cut -d '2' -f1 "  # Replace with your desired command
+    crt_month = "/usr/bin/openssl x509 -enddate -noout -in /opt/Dashboard-linux/project1/extra/clients.crt  | awk -F '=' '{print $2}' | cut -d '2' -f1 "  # Replace with your desired command
     crt_month_result = subprocess.check_output(crt_month, shell=True)
     crt_month_tmp = crt_month_result.decode('utf-8').rstrip()
     if crt_month_tmp in month_dict.keys():
